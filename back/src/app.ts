b30a62784cf2b81bc763ea7455b2be8f1de2e556
César <cesarrdev@gmail.com>
2023-09-29 00:40:29 -0300

    feat: errorHandler function created

--- back/src/app.ts
+++ back/src/app.ts
@@ -2,14 +2,16 @@ import "express-async-errors"
 import "reflect-metadata"
 import express from "express"
 import cors from "cors"
+import { userRoutes } from "./routes/user.routes"
+import { errorHandler } from "./errors/errorHandler.errors"
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.get("/", (req, res) => {
-    res.send("Hello World!")
-})
+app.use("/user",userRoutes)
+
+app.use(errorHandler)
 
 export default app
 
