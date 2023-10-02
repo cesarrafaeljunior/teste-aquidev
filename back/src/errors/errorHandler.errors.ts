352f235aa05ae835f01b56d5107fa29a1c197493
César <cesarrdev@gmail.com>
2023-10-02 15:06:58 -0300

    fix:errors zod validation

--- back/src/errors/errorHandler.errors.ts
+++ back/src/errors/errorHandler.errors.ts
@@ -1,27 +1,28 @@
-import {Request, Response, NextFunction} from "express"
-import { AppError } from "./AppError.errors"
-import {ZodError} from "zod"
+import { Request, Response, NextFunction } from "express";
+import { AppError } from "./AppError.errors";
+import { ZodError } from "zod";
+import { z } from "zod";
 
 export const errorHandler = async (
-    error: Error,
-    req: Request,
-    res: Response,
-    next:NextFunction
+  error: Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
 ) => {
-    if(error instanceof AppError){
-        return res.status(error.statusCode).json({
-            message: error.message
-        })
-    }
+  if (error instanceof AppError) {
+    return res.status(error.statusCode).json({
+      message: error.message,
+    });
+  }
 
-    if(error instanceof ZodError){
-        return res.status(400).json({
-            message: error.flatten().fieldErrors
-        })
-    }
-    
-    console.log(error);
-    return res.status(500).json({
-        message: "Internal server error"
-    })
-}
+  if (error instanceof ZodError) {
+    return res.status(400).json({
+      message: error.flatten().fieldErrors,
+    });
+  }
+
+  console.log(error);
+  return res.status(500).json({
+    message: "Internal server error",
+  });
+};
