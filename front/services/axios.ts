d3c69f25754f899b94993d8f7be26ed35c2aa918
César <cesarrdev@gmail.com>
2023-10-02 21:04:51 -0300

    feat: Dynamic PORT environment variable

--- front/services/axios.ts
+++ front/services/axios.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import "dotenv/config"
 
 export const instance = axios.create({
     baseURL: 'https://api.punkapi.com/v2/',
@@ -7,7 +8,7 @@ export const instance = axios.create({
 })
 
 export const localApi = axios.create({
-    baseURL: 'http://localhost:3001/',
+    baseURL: `http://localhost:${process.env.PORTAPI || 3001}/`,
     timeout: 1000,
     headers: { "Content-Type": "application/json" },
 })
