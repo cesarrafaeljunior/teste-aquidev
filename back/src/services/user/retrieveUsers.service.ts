46276672a7a378145f5abf9447c5122b5344eddb
César <cesarrdev@gmail.com>
2023-10-02 23:32:16 -0300

    fix: return users no password

--- back/src/services/user/retrieveUsers.service.ts
+++ back/src/services/user/retrieveUsers.service.ts
@@ -1,9 +1,13 @@
-import prisma from "../../database/connect.database"
-import { iUserResponse } from "../../interfaces/user.interface"
+import prisma from "../../database/connect.database";
+import { iUserResponse } from "../../interfaces/user.interface";
+import { returnUserSchemaWithoutPassword } from "../../schemas/user.schemas";
 
 export const retrieveUsersService = async (): Promise<iUserResponse[]> => {
+  const users = await prisma.user.findMany();
 
-    const users = await prisma.user.findMany()
+  const userResponse: iUserResponse[] = users.map((user) => {
+    return returnUserSchemaWithoutPassword.parse(user);
+  });
 
-    return users
-}
+  return userResponse;
+};
